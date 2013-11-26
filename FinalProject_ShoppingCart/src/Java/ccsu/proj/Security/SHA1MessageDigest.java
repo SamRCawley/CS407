852b44b2dd85b6f511d536ea1074d40a3e20ebf8
Samuel Cawley <samrcawley@gmail.com>
2013-11-26 11:37:56 -0500

    Shopping cart add/remove functionality works, but something in the code to bring up cart.xhtml causes the button to not call the remove function on that page.
    Currently cart is being displayed on index.xhtml until cart.xhtml is fixed

--- FinalProject_ShoppingCart/src/Java/ccsu/proj/Security/SHA1MessageDigest.java
+++ FinalProject_ShoppingCart/src/Java/ccsu/proj/Security/SHA1MessageDigest.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package ccsu.proj.Classes;
+package ccsu.proj.Security;
 
 import java.security.MessageDigest;
 
