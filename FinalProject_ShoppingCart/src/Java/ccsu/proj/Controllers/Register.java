e93a69561d846bcd0bc568afc01739078b7afb3b
Jason <Jason@149.152.101.109>
2013-11-29 20:03:06 -0500

    Registration now works

--- FinalProject_ShoppingCart/src/Java/ccsu/proj/Controllers/Register.java
+++ FinalProject_ShoppingCart/src/Java/ccsu/proj/Controllers/Register.java
@@ -10,6 +10,7 @@ import javax.annotation.Resource;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.SessionScoped;
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.PersistenceUnit;
 import javax.transaction.UserTransaction;
@@ -25,18 +26,50 @@ public class Register implements Serializable {
     private EntityManagerFactory entityManagerFactory;
     @Resource
     private UserTransaction userTransaction;
-    //@ManagedProperty(value = "#{account}")
+    @ManagedProperty(value = "#{account}")
     private Account account;
     
     public Register() {
     
     }
     
-    public void registerNewUser() {
-        
+    public String registerNewUser() {
+        String returnValue = "registrationError";
+        try {
+            incrementAccountID(); //Make sure account id is not zero
+            userTransaction.begin();
+            EntityManager em = entityManagerFactory.createEntityManager();
+            em.persist(account);
+            userTransaction.commit();
+            em.close();
+            returnValue = "registrationSuccess";
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return returnValue;
     }
     
-    //An empty controller at the moment
+    private void incrementAccountID() {
+        int newID = 0;
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        String selectSQL = "select max(id) from Account"; 
+        
+        try {
+            newID = (Integer)entityManager.createQuery("select max(a.id) from Account a").getSingleResult() + 1;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        account.setId(newID);
+    }
+    
+    public void setAccount(Account account) {
+        this.account = account;
+    }
+    
+    public Account getAccount() {
+        return this.account;
+    }
 }
 
 
--- FinalProject_ShoppingCart/src/Java/ccsu/proj/Validators/UsernameValidator.java
+++ FinalProject_ShoppingCart/src/Java/ccsu/proj/Validators/UsernameValidator.java
@@ -17,9 +17,34 @@ import javax.faces.validator.ValidatorException;
 
 @FacesValidator(value="usernameValidator")
 public class UsernameValidator implements Validator {
+    //private EntityManagerFactory entityManagerFactory;
     
     @Override
     public void validate(FacesContext context, UIComponent component, Object value) throws ValidatorException {
         /* Validate username. Makes sure name isn't already taken. Might be able to just use Login bean */
+        String username = value.toString();
+        
+//        if (usernameExists(username)) {
+//            FacesMessage facesMessage =  new FacesMessage("Username is already taken");
+//            throw new ValidatorException(facesMessage);
+//        }
     }
+    
+//    public boolean usernameExists(String username) {
+//        List<Account> accounts = new ArrayList();
+//        EntityManager entityManager = entityManagerFactory.createEntityManager();
+//        String selectSQL = "select a from Account a where a.username = :username"; 
+//        
+//        try {
+//            Query selectQuery = entityManager.createQuery(selectSQL);
+//            selectQuery.setParameter("username", username);
+//            accounts = selectQuery.getResultList();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//        if(accounts.size() == 1)
+//            return true;
+//        else
+//            return false;
+//    }
 }
