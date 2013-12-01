64fb3dcfbe50442d2131137fb48073760972298d
JGR <JGR@192.168.0.2>
2013-11-30 22:33:38 -0500

    more named queries

--- FinalProject_ShoppingCart/src/Java/ccsu/proj/Model/Categories.java
+++ FinalProject_ShoppingCart/src/Java/ccsu/proj/Model/Categories.java
@@ -6,12 +6,18 @@ package ccsu.proj.Model;
 
 import java.io.Serializable;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
 /**
@@ -22,14 +28,24 @@ import javax.persistence.Table;
 @ManagedBean
 @Entity
 @Table(name="CATEGORIES")
+@ViewScoped
+@NamedQueries({
+    @NamedQuery(name = "Categories.findAll", query = "SELECT c FROM Categories c"),
+    @NamedQuery(name = "Categories.byID", query = "SELECT c FROM Categories c WHERE c.cid = :number"),
+    @NamedQuery(name = "Categories.byName", query = "SELECT c FROM Categories c WHERE c.categoryName LIKE :categoryname")})
 public class Categories implements Serializable {
+    @JoinTable(name = "PRODUCTS_CATEGORIES", joinColumns = {
+        @JoinColumn(name = "CID", referencedColumnName = "CID")}, inverseJoinColumns = {
+        @JoinColumn(name = "PID", referencedColumnName = "ID")})
+    @ManyToMany
+    private List<Products> productsList;
 
     @Id
     @Column(name="CID")
-    private int cid = 0;
+    private int cid;;
     
     @Column(name="CATEGORYNAME")
-    private String categoryName = null;
+    private String categoryName;
     
     @ManyToMany(mappedBy="categories")
     private Set<Products> products = new HashSet();
@@ -64,6 +80,14 @@ public class Categories implements Serializable {
     public void setProducts(Set<Products> products)
     {
         this.products = products;
+    }
+
+    public List<Products> getProductsList() {
+        return productsList;
+    }
+
+    public void setProductsList(List<Products> productsList) {
+        this.productsList = productsList;
     }
     
 }
